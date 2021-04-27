d2f6e44cb24c8aa89ffd3e8eb75cd8df683f2317
LoftusN13 <loftus.niamh@yahoo.ie>
2021-04-27 20:22:08 +0000

    Display msg to user on submission of contact form

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -4,6 +4,16 @@ function sendEmail(contactForm){
         "from_email": contactForm.email.value,
         "user_message": contactForm.message.value
     })
+    .then(
+        /* Alert user to successful form submission */
+        function(response) {
+            document.getElementById("submit-msg").innerHTML = "It's on the way! Your message has been sent!";
+        },
+        /* Alert user to unsuccessful form submission */
+        function(error) {
+             document.getElementById("submit-msg").innerHTML = "Uh-oh! Something went wrong; message has not been sent!";
+        }
+    );
 
     document.getElementById("contactForm").reset();
     return false;
