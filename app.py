066addb69674e3bbcd54e8087a219ebfa4af552c
LoftusN13 <loftus.niamh@yahoo.ie>
2021-05-03 19:57:28 +0000

    Switch debug to false

--- app.py
+++ app.py
@@ -309,4 +309,4 @@ def cocktail_recipe(recipe_id):
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
